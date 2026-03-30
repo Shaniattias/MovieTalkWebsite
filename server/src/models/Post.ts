c55dd6fe2ea470839699b73c436aa4970b196c18
taliawachter <talia1811v@gmail.com>
2026-03-30 18:19:11 +0300

    feat(posts): add Post mongoose model

---
+++ server/src/models/Post.ts
@@ -0,0 +1,18 @@
+import { Schema, model, Document, Types } from "mongoose";
+
+export interface IPost extends Document {
+  author: Types.ObjectId;
+  title: string;
+  text: string;
+}
+
+const postSchema = new Schema<IPost>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    title: { type: String, required: true, trim: true },
+    text: { type: String, required: true, trim: true },
+  },
+  { timestamps: true }
+);
+
+export const Post = model<IPost>("Post", postSchema);
